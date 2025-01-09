9c098cd5193340a0fd47b30ac3ade3a4957ed772
Abhinav Goyal <abhinavgoel@Admins-MacBook-Air.local>
2025-01-09 12:05:18 +0530

    updated loki logging config

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,35 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
 import reportRouter from "./routes/reportRoute";
 import { RedisService } from "ondc-automation-cache-lib";
 import { logger } from "./utils/logger";
 
-import dotenv from "dotenv"; // Import dotenv to load environment variables
+// Initialize dotenv to load environment variables
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-dotenv.config(); // Load environment variables from the .env file
+
+// Set up Redis database connection
 try {
   RedisService.useDb(2);
 } catch (err) {
   logger.error(err);
 }
 
+// Middleware setup
 app.use(express.json());
+
+// Routes
 app.use("/generate-report", reportRouter);
 
-app.use((err: any, req: Request, res: Response, next: any) => {
+// Global error handler middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   logger.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
 
+// Start the server
 app.listen(PORT, () => {
   logger.info(`Server is running on http://localhost:${PORT}`);
 });
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,4 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from "winston";
 const { combine, timestamp, printf, colorize } = format;
 import LokiTransport from "winston-loki";
 
@@ -9,22 +9,15 @@ const customFormat = printf(({ level, message, timestamp }) => {
 
 // Create a logger instance
 export const logger = createLogger({
-  level: 'info', // Default log level
+  level: "info", // Default log level
   format: combine(
-    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     colorize(), // Adds color to log levels (e.g., error in red, info in green)
     customFormat
   ),
   transports: [
     new transports.Console(), // Log to the console
-    new transports.File({ filename: 'logs/application.log' }), // Log to a file
-    new LokiTransport({
-      host: "http://localhost:3100", // Loki instance URL
-      labels: { app: "automation-reporting-service" }, // Custom labels for Loki logs
-      json: true, // Send logs in JSON format
-      interval: 1, // Flush logs every 1 second
-      // replaceTimestamp: true, // Replace timestamps with formatted ones
-    }),
+    new transports.File({ filename: "logs/application.log" }), // Log to a file
   ],
 });
 
