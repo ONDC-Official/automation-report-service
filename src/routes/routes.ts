2851b9fec170f48392199f5400d8da691ae8c22b
Shivang Dudani <shivangdudani113@gmail.com>
2025-10-09 11:16:49 +0530

    small change

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"; 
 import apiKeyMiddleware from "../middleware/api-key"; // Import API key middleware
 import ReportRoutes from "./reportRoutes"; // Import report routes
-import LogRoutes from "./reportRoutes";
+import LogRoutes from "./logRoutes";
 const router = Router();
 
 // Mount session and payload related routes
