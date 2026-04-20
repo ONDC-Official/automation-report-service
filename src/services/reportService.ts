120a912f2667867e48ab80546c04cc20733562b4
ParneetkaurDev <parneet.kaur@ondc.org>
2026-04-20 10:07:33 +0530

    changes in unified credit

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -82,7 +82,7 @@ export class ReportService {
       return htmlReport;
     } catch (error) {
       logger.error(
-        `Error generating report for session ${sessionId}:`,
+        `Error generating report for session id${sessionId}:`,
         {},
         error
       );
