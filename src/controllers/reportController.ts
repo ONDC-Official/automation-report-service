6ce5813bd83c69678fda394b3dcb44ae51c5cc83
Shivang Dudani <shivangdudani113@gmail.com>
2026-05-15 15:55:17 +0530

    making userId user_id for frontend compatilbility

--- src/controllers/reportController.ts
+++ src/controllers/reportController.ts
@@ -10,7 +10,7 @@ export async function generateReportController(
 ): Promise<void> {
   try {
     const sessionId = req.query.sessionId as string;
-    const userId = req.query?.userId as string;
+    const userId = req.query?.user_id as string;
     if (!sessionId) {
       logger.error(MESSAGES.responses.missingSessionId);
       apiResponse.badRequest(res,MESSAGES.responses.missingSessionId);
