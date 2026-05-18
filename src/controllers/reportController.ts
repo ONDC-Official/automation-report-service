84b9f73836f25e6dd759076a819d9c676da2f3e8
Shivang Dudani <shivangdudani113@gmail.com>
2026-05-18 11:48:03 +0530

    logger test

--- src/controllers/reportController.ts
+++ src/controllers/reportController.ts
@@ -11,6 +11,7 @@ export async function generateReportController(
   try {
     const sessionId = req.query.sessionId as string;
     const userId = req.query?.user_id as string;
+    logger.info("Received request to generate report for sessionId:", { sessionId, userId });
     if (!sessionId) {
       logger.error(MESSAGES.responses.missingSessionId);
       apiResponse.badRequest(res,MESSAGES.responses.missingSessionId);
