8858f00126da200cd2bf820c0ecf570e410ea20a
vaibhavwits <vaibhav.sharma2@thewitslab.com>
2026-07-07 13:35:23 +0530

    fix:(minor fix)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -613,6 +613,17 @@ export const FLOW_ID_MAP: Record<
       },
     },
   },
+  "ONDC:RETINVL": {
+    "1.2.5": {
+      "INVL": {
+        "RETINVL_DISCOVERY_FLOW": "RETINVL_DISCOVERY_FLOW",
+        "ORDER_FLOW_BASELINE": "ORDER_FLOW_BASELINE",
+        "ORDER_FLOW_BASELINE_ILBP": "ORDER_FLOW_BASELINE_ILBP",
+        "OFFERS_FLOW":"OFFERS_FLOW", 
+        "BUYER_SIDE_ORDER_CANCELLATION":"BUYER_SIDE_ORDER_CANCELLATION",
+      },
+    },
+  },
   "ONDC:LOG10": {
     "1.2.5": {
       "ride-Logistics (P2P)": {
@@ -688,7 +699,8 @@ export const typeMapping: Record<string, string> = {
   "transit-insurance": "SACHET_INSURANCE",
   "accidental-insurance": "SACHET_INSURANCE",
   "hospicash-insurance": "SACHET_INSURANCE",
-  "Logistics (NIC2004:60232)":"LOGISTICS"
+  "Logistics (NIC2004:60232)":"LOGISTICS",
+  "RETINVL":"RETINVL"
 };
 
 export const NIC_LOGISTICS_CATEGORY_FLOW_MAP: Record<string, string> = {
